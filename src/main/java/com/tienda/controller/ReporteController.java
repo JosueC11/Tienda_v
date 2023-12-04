cc130edabff35587c46468cd2fb1a3f1dfe21732
JosueC11 <jcastillor1104@gmail.com>
2023-12-03 19:25:32 -0600

    Agregando S12 y Tarea 5 Bien jsjs
    Reportes Categoria y Productos

---
+++ src/main/java/com/tienda/controller/ReporteController.java
@@ -0,0 +1,51 @@
+
+package com.tienda.controller;
+
+import com.tienda.service.ReporteService;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/reportes")
+public class ReporteController 
+{
+    @Autowired
+    ReporteService reporteService;
+    
+    @GetMapping("/principal")
+    public String principal(Model model)
+    {
+        return "/reportes/principal";
+    }   
+    
+    @GetMapping("/usuarios")
+    public ResponseEntity<Resource> usuarios(@RequestParam String tipo) throws IOException
+    {
+        return reporteService.generarReporte("usuarios",null,tipo);
+    }
+    
+    @GetMapping("/ventas")
+    public ResponseEntity<Resource> ventas(@RequestParam String tipo) throws IOException
+    {
+        return reporteService.generarReporte("ventas",null,tipo);
+    }
+    
+    @GetMapping("/productos")
+    public ResponseEntity<Resource> productos(@RequestParam String tipo) throws IOException
+    {
+        return reporteService.generarReporte("productos",null,tipo);
+    }
+    
+    @GetMapping("/categorias")
+    public ResponseEntity<Resource> categorias(@RequestParam String tipo) throws IOException
+    {
+        return reporteService.generarReporte("categorias",null,tipo);
+    }
+}
